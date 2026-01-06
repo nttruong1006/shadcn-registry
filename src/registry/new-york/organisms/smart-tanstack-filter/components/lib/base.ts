7d02953af5b574b24c42b4eb7c8de1e629beb69e
Nguyễn Thế Trường <nttruong1006@gmail.com>
2026-01-06 15:32:44 +0700

    feat(smart tanstack filter): transform form value to api filters param util

--- src/registry/new-york/organisms/smart-tanstack-filter/components/lib/base.ts
+++ src/registry/new-york/organisms/smart-tanstack-filter/components/lib/base.ts
@@ -1,3 +1,6 @@
+import { type DateArg, endOfDay, endOfMonth, endOfYear, startOfDay, startOfMonth, startOfYear } from 'date-fns'
+import type { AdvancedFilterFormValueOutput, BasicSearchFormValueOutput } from './form'
+
 // Smart filter logical operation
 export enum SmartFilterLogicalOperation {
   And = ',',
@@ -116,5 +119,122 @@ export interface Filter {
     label: string
   }>
   apiPath?: string
-  datePickerFormat?: 'date' | 'month' | 'year'
+  dateFormat?: 'date' | 'month' | 'year'
+}
+
+// Transform form value to api filters param
+const periodHandlerPerDateFormat: Record<
+  NonNullable<Filter['dateFormat']>,
+  {
+    start: (date: DateArg<Date>) => Date
+    end: (date: DateArg<Date>) => Date
+  }
+> = {
+  date: { start: startOfDay, end: endOfDay },
+  month: { start: startOfMonth, end: endOfMonth },
+  year: { start: startOfYear, end: endOfYear }
+}
+
+export function transformFormValueToApiFiltersParam(
+  value: BasicSearchFormValueOutput['keyword'] | AdvancedFilterFormValueOutput['filters'],
+  filters: Filter[],
+  handler?: {
+    basicSearch?: string | ((value: BasicSearchFormValueOutput['keyword']) => string)
+    advancedFilter?: (value: AdvancedFilterFormValueOutput['filters']) => string
+  }
+) {
+  // Base filter
+  if (typeof value === 'string') {
+    if (value === '') {
+      return undefined
+    }
+
+    if (typeof handler?.basicSearch === 'function') {
+      return handler.basicSearch(value)
+    }
+
+    const baseFilterKey =
+      handler?.basicSearch && typeof handler.basicSearch === 'string' ? handler.basicSearch : 'value'
+    return `${baseFilterKey}${SmartFilterApiOperation.Contain}${value}`
+  }
+
+  // Advanced filter
+  const filtersMap = filters.reduce<Record<string, Omit<Filter, 'name'>>>((acc, filter) => {
+    acc[filter.name] = { ...filter }
+    return acc
+  }, {})
+
+  return handler?.advancedFilter
+    ? handler.advancedFilter(value)
+    : value
+        .map(({ name, operation, value }, index) => {
+          const logicalOperation = index > 0 ? SmartFilterLogicalOperation.And : ''
+          const { type, dateFormat = 'date' } = filtersMap[name]
+
+          // Common
+          // Has any of operation
+          if (Array.isArray(value.default) && operation === SmartFilterOperation.HasAnyOf) {
+            return `${logicalOperation}${name}${SmartFilterApiOperation.Equal}${value.default.join(SmartFilterLogicalOperation.Or)}`
+          }
+
+          // Has all of operation
+          if (Array.isArray(value.default) && operation === SmartFilterOperation.HasAllOf) {
+            return value.default
+              .map((item) => `${logicalOperation}${name}${SmartFilterApiOperation.Equal}${item}`)
+              .join(SmartFilterLogicalOperation.And)
+          }
+
+          switch (type) {
+            case SmartFilterType.Number: {
+              // Is between operation
+              if (operation === SmartFilterOperation.IsBetween) {
+                return `${logicalOperation}${name}${SmartFilterApiOperation.GreaterThanOrEqual}${value.additional.from}${SmartFilterLogicalOperation.And}${name}${SmartFilterApiOperation.LessThanOrEqual}${value.additional.to}`
+              }
+              break
+            }
+
+            case SmartFilterType.Date: {
+              const dateValue = value.default as string
+
+              // Equals operation
+              if (operation === SmartFilterOperation.EqualsTo) {
+                return `${logicalOperation}${name}${SmartFilterApiOperation.GreaterThanOrEqual}${periodHandlerPerDateFormat[dateFormat].start(dateValue).toISOString()}${SmartFilterLogicalOperation.And}${name}${SmartFilterApiOperation.LessThanOrEqual}${periodHandlerPerDateFormat[dateFormat].end(dateValue).toISOString()}`
+              }
+
+              // Is less than operation
+              if (operation === SmartFilterOperation.IsLessThan) {
+                return `${logicalOperation}${name}${apiOperationPerOperation[operation]}${periodHandlerPerDateFormat[dateFormat].start(dateValue).toISOString()}`
+              }
+
+              // Is less than or equal to operation
+              if (operation === SmartFilterOperation.IsLessThanOrEqualTo) {
+                return `${logicalOperation}${name}${apiOperationPerOperation[operation]}${periodHandlerPerDateFormat[dateFormat].end(dateValue).toISOString()}`
+              }
+
+              // Is greater than operation
+              if (operation === SmartFilterOperation.IsGreaterThan) {
+                return `${logicalOperation}${name}${apiOperationPerOperation[operation]}${periodHandlerPerDateFormat[dateFormat].end(dateValue).toISOString()}`
+              }
+
+              // Is greater than or equal to operation
+              if (operation === SmartFilterOperation.IsGreaterThanOrEqualTo) {
+                return `${logicalOperation}${name}${apiOperationPerOperation[operation]}${periodHandlerPerDateFormat[dateFormat].start(dateValue).toISOString()}`
+              }
+
+              // Is between operation
+              if (operation === SmartFilterOperation.IsBetween) {
+                return `${logicalOperation}${name}${SmartFilterApiOperation.GreaterThanOrEqual}${periodHandlerPerDateFormat[dateFormat].start(value.additional.from).toISOString()}${SmartFilterLogicalOperation.And}${name}${SmartFilterApiOperation.LessThanOrEqual}${periodHandlerPerDateFormat[dateFormat].end(value.additional.to).toISOString()}`
+              }
+
+              break
+            }
+
+            default:
+              break
+          }
+
+          // Normal filter
+          return `${logicalOperation}${name}${apiOperationPerOperation[operation]}${value.default}`
+        })
+        .join('')
 }
--- src/registry/new-york/organisms/smart-tanstack-filter/examples/demo.tsx
+++ src/registry/new-york/organisms/smart-tanstack-filter/examples/demo.tsx
@@ -1,53 +1,57 @@
 import React from 'react'
-import { SmartFilter, type SmartFilterProps, SmartFilterType } from '@/components/organisms/smart-tanstack-filter'
+import {
+  type Filter,
+  SmartFilter,
+  type SmartFilterProps,
+  SmartFilterType,
+  transformFormValueToApiFiltersParam
+} from '@/components/organisms/smart-tanstack-filter'
 
 // Component
+const filters: Filter[] = [
+  {
+    name: 'fullName',
+    label: 'Full name',
+    type: SmartFilterType.Input
+  },
+  {
+    name: 'age',
+    label: 'Age',
+    type: SmartFilterType.Number
+  },
+  {
+    name: 'graduationDate',
+    label: 'Graduation date',
+    type: SmartFilterType.Date
+  },
+  {
+    name: 'department',
+    label: 'Department',
+    type: SmartFilterType.SelectWithOptions,
+    options: [
+      { value: 'front-end', label: 'Front-end' },
+      { value: 'back-end', label: 'Back-end' }
+    ]
+  },
+  {
+    name: 'technologies',
+    label: 'Technologies',
+    type: SmartFilterType.SelectWithQuery,
+    apiPath: '/version/1.0/options/role',
+    options: [
+      { value: 'react', label: 'React' },
+      { value: 'tailwind-css', label: 'TailwindCSS' },
+      { value: 'astro', label: 'Astro' },
+      { value: 'ts', label: 'TypeScript' }
+    ]
+  }
+]
+
 export const SmartFilterDemo = () => {
   // Methods
   const setFilters: SmartFilterProps['setFilters'] = React.useCallback((formValue) => {
-    console.log(formValue)
-  }, [])
-
-  // Memos
-  const filters = React.useMemo<SmartFilterProps['filters']>(() => {
-    return [
-      {
-        name: 'fullName',
-        label: 'Full name',
-        type: SmartFilterType.Input
-      },
-      {
-        name: 'age',
-        label: 'Age',
-        type: SmartFilterType.Number
-      },
-      {
-        name: 'graduationDate',
-        label: 'Graduation date',
-        type: SmartFilterType.Date
-      },
-      {
-        name: 'department',
-        label: 'Department',
-        type: SmartFilterType.SelectWithOptions,
-        options: [
-          { value: 'front-end', label: 'Front-end' },
-          { value: 'back-end', label: 'Back-end' }
-        ]
-      },
-      {
-        name: 'technologies',
-        label: 'Technologies',
-        type: SmartFilterType.SelectWithQuery,
-        apiPath: '/version/1.0/options/role',
-        options: [
-          { value: 'react', label: 'React' },
-          { value: 'tailwind-css', label: 'TailwindCSS' },
-          { value: 'astro', label: 'Astro' },
-          { value: 'ts', label: 'TypeScript' }
-        ]
-      }
-    ]
+    const filtersParam = transformFormValueToApiFiltersParam(formValue, filters)
+    console.log(filtersParam)
   }, [])
 
   // Template
