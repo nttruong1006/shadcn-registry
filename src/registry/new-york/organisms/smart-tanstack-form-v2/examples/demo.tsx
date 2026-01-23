af7f0e3df529a52f1ed9324dffd41222c77ed336
Nguyễn Thế Trường <nttruong1006@gmail.com>
2026-01-23 16:03:16 +0700

    feat(smart tanstack form v2): example demo

--- src/registry/new-york/organisms/smart-tanstack-form-v2/examples/demo.tsx
+++ src/registry/new-york/organisms/smart-tanstack-form-v2/examples/demo.tsx
@@ -142,14 +142,15 @@ export const SmartFormDemo = () => {
               form.handleSubmit()
             }}
           >
-            {/* Personal */}
-            <FieldSet>
-              <FieldLegend>Personal</FieldLegend>
-              <FieldDescription>Fill personal information</FieldDescription>
+            <form.AppForm>
+              {/* Personal */}
+              <FieldSet>
+                <FieldLegend>Personal</FieldLegend>
+                <FieldDescription>Fill personal information</FieldDescription>
 
-              {/* Form template fields */}
-              <div className='grid grid-cols-3 gap-x-4 gap-y-6'>
-                {/* <form.AppField
+                {/* Form template fields */}
+                <div className='grid grid-cols-3 gap-x-4 gap-y-6'>
+                  {/* <form.AppField
                   name='province'
                   listeners={{
                     onChangeDebounceMs: 400,
@@ -168,135 +169,142 @@ export const SmartFormDemo = () => {
                   )}
                 </form.AppField> */}
 
-                <form.AppField name='fullName'>{(field) => <field.Input label='Full name' isRequired />}</form.AppField>
-                <form.AppField name='age'>{(field) => <field.Number label='Age' isRequired />}</form.AppField>
-                <form.AppField name='birthdate'>{(field) => <field.Date label='Birthdate' isRequired />}</form.AppField>
-                <form.AppField name='gender'>
-                  {(field) => (
-                    <field.SelectWithOptions
-                      label='Gender'
-                      isRequired
-                      options={[
-                        { value: 'male', label: 'Male' },
-                        { value: 'female', label: 'Female' }
-                      ]}
-                    />
+                  <form.AppField name='fullName'>
+                    {(field) => <field.Input label='Full name' isRequired />}
+                  </form.AppField>
+                  <form.AppField name='age'>{(field) => <field.Number label='Age' isRequired />}</form.AppField>
+                  <form.AppField name='birthdate'>
+                    {(field) => <field.Date label='Birthdate' isRequired />}
+                  </form.AppField>
+                  <form.AppField name='gender'>
+                    {(field) => (
+                      <field.SelectWithOptions
+                        label='Gender'
+                        isRequired
+                        options={[
+                          { value: 'male', label: 'Male' },
+                          { value: 'female', label: 'Female' }
+                        ]}
+                      />
+                    )}
+                  </form.AppField>
+                  <form.AppField name='phoneNumber'>
+                    {(field) => <field.PhoneNumber label='Phone number' isRequired />}
+                  </form.AppField>
+                  <form.AppField name='email'>{(field) => <field.Input label='Email' isRequired />}</form.AppField>
+                  <form.AppField name='description'>
+                    {(field) => <field.Textarea label='Description' isRequired className='col-span-full' />}
+                  </form.AppField>
+                </div>
+              </FieldSet>
+
+              {/* Professional skills */}
+              <FieldSet>
+                <FieldLegend>Professional skills</FieldLegend>
+                <FieldDescription>Fill professional skills</FieldDescription>
+
+                {/* Form template fields */}
+                <div className='grid grid-cols-3 gap-x-4 gap-y-6'>
+                  <form.AppField name='department'>
+                    {(field) => (
+                      <field.SelectWithOptions
+                        label='Department'
+                        isRequired
+                        options={[
+                          { value: 'development', label: 'Development' },
+                          { value: 'design', label: 'Design' },
+                          { value: 'marketing', label: 'Marketing' }
+                        ]}
+                      />
+                    )}
+                  </form.AppField>
+                  <form.AppField name='technologies'>
+                    {(field) => (
+                      <field.MultiSelectWithOptions
+                        label='Technologies'
+                        isRequired
+                        options={[
+                          { value: 'react', label: 'React' },
+                          { value: 'nextjs', label: 'Next.js' },
+                          { value: 'tailwindcss', label: 'Tailwind CSS' },
+                          { value: 'typescript', label: 'TypeScript' }
+                        ]}
+                      />
+                    )}
+                  </form.AppField>
+                  <form.AppField name='graduatedUniversity'>
+                    {(field) => (
+                      <field.AutocompleteWithOptions
+                        label='Graduated university'
+                        isRequired
+                        options={[
+                          {
+                            value: 'TDTU',
+                            label: 'Ton Duc Thang University'
+                          },
+                          {
+                            value: 'VLU',
+                            label: 'Van Lang University'
+                          },
+                          {
+                            value: 'UIT',
+                            label: 'University of information technology'
+                          }
+                        ]}
+                      />
+                    )}
+                  </form.AppField>
+                  <form.AppField name='resumes'>
+                    {(field) => <field.MultiFile label='Resumes' isRequired className='col-span-full' />}
+                  </form.AppField>
+                  <form.AppField name='isDeepKnowledge'>
+                    {(field) => <field.Checkbox label='Is deep knowledge' />}
+                  </form.AppField>
+                </div>
+              </FieldSet>
+
+              {/* Account */}
+              <FieldSet>
+                <FieldLegend>Account</FieldLegend>
+                <FieldDescription>Fill account information</FieldDescription>
+
+                {/* Form template fields */}
+                <div className='grid grid-cols-3 gap-x-4 gap-y-6'>
+                  <form.AppField name='username'>
+                    {(field) => <field.Input label='Username' isRequired />}
+                  </form.AppField>
+                  <form.AppField name='password'>
+                    {(field) => <field.Password label='Password' isRequired />}
+                  </form.AppField>
+                  <form.AppField name='passwordConfirmation'>
+                    {(field) => <field.Password label='Password confirmation' isRequired />}
+                  </form.AppField>
+                  <form.AppField name='hobby'>
+                    {(field) => <field.Editor label='Hobby' isRequired className='col-span-full' />}
+                  </form.AppField>
+                </div>
+              </FieldSet>
+
+              {/* Action buttons */}
+              <div className='flex flex-col justify-stretch gap-4 xl:flex-row xl:justify-end'>
+                <Button
+                  variant='outline'
+                  onClick={() => {
+                    form.reset()
+                  }}
+                >
+                  Cancel
+                </Button>
+
+                <form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
+                  {([canSubmit, isSubmitting]) => (
+                    <Button type='submit' form={form.formId} disabled={!canSubmit} isLoading={isSubmitting}>
+                      Submit
+                    </Button>
                   )}
-                </form.AppField>
-                <form.AppField name='phoneNumber'>
-                  {(field) => <field.PhoneNumber label='Phone number' isRequired />}
-                </form.AppField>
-                <form.AppField name='email'>{(field) => <field.Input label='Email' isRequired />}</form.AppField>
-                <form.AppField name='description'>
-                  {(field) => <field.Textarea label='Description' isRequired className='col-span-full' />}
-                </form.AppField>
+                </form.Subscribe>
               </div>
-            </FieldSet>
-
-            {/* Professional skills */}
-            <FieldSet>
-              <FieldLegend>Professional skills</FieldLegend>
-              <FieldDescription>Fill professional skills</FieldDescription>
-
-              {/* Form template fields */}
-              <div className='grid grid-cols-3 gap-x-4 gap-y-6'>
-                <form.AppField name='department'>
-                  {(field) => (
-                    <field.SelectWithOptions
-                      label='Department'
-                      isRequired
-                      options={[
-                        { value: 'development', label: 'Development' },
-                        { value: 'design', label: 'Design' },
-                        { value: 'marketing', label: 'Marketing' }
-                      ]}
-                    />
-                  )}
-                </form.AppField>
-                <form.AppField name='technologies'>
-                  {(field) => (
-                    <field.MultiSelectWithOptions
-                      label='Technologies'
-                      isRequired
-                      options={[
-                        { value: 'react', label: 'React' },
-                        { value: 'nextjs', label: 'Next.js' },
-                        { value: 'tailwindcss', label: 'Tailwind CSS' },
-                        { value: 'typescript', label: 'TypeScript' }
-                      ]}
-                    />
-                  )}
-                </form.AppField>
-                <form.AppField name='graduatedUniversity'>
-                  {(field) => (
-                    <field.AutocompleteWithOptions
-                      label='Graduated university'
-                      isRequired
-                      options={[
-                        {
-                          value: 'TDTU',
-                          label: 'Ton Duc Thang University'
-                        },
-                        {
-                          value: 'VLU',
-                          label: 'Van Lang University'
-                        },
-                        {
-                          value: 'UIT',
-                          label: 'University of information technology'
-                        }
-                      ]}
-                    />
-                  )}
-                </form.AppField>
-                <form.AppField name='resumes'>
-                  {(field) => <field.MultiFile label='Resumes' isRequired className='col-span-full' />}
-                </form.AppField>
-                <form.AppField name='isDeepKnowledge'>
-                  {(field) => <field.Checkbox label='Is deep knowledge' />}
-                </form.AppField>
-              </div>
-            </FieldSet>
-
-            {/* Account */}
-            <FieldSet>
-              <FieldLegend>Account</FieldLegend>
-              <FieldDescription>Fill account information</FieldDescription>
-
-              {/* Form template fields */}
-              <div className='grid grid-cols-3 gap-x-4 gap-y-6'>
-                <form.AppField name='username'>{(field) => <field.Input label='Username' isRequired />}</form.AppField>
-                <form.AppField name='password'>
-                  {(field) => <field.Password label='Password' isRequired />}
-                </form.AppField>
-                <form.AppField name='passwordConfirmation'>
-                  {(field) => <field.Password label='Password confirmation' isRequired />}
-                </form.AppField>
-                <form.AppField name='hobby'>
-                  {(field) => <field.Editor label='Hobby' isRequired className='col-span-full' />}
-                </form.AppField>
-              </div>
-            </FieldSet>
-
-            {/* Action buttons */}
-            <div className='flex flex-col justify-stretch gap-4 xl:flex-row xl:justify-end'>
-              <Button
-                variant='outline'
-                onClick={() => {
-                  form.reset()
-                }}
-              >
-                Cancel
-              </Button>
-
-              <form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
-                {([canSubmit, isSubmitting]) => (
-                  <Button type='submit' form={form.formId} disabled={!canSubmit} isLoading={isSubmitting}>
-                    Submit
-                  </Button>
-                )}
-              </form.Subscribe>
-            </div>
+            </form.AppForm>
           </form>
         </DialogScrollableContent>
       </DialogContent>
