5543659632acb3c2bbf8ebc76b5d9541fb15ff12
nttruong21 <nttruong1006@gmail.com>
2026-04-15 10:55:26 +0700

    feat(color-picker): update ui base on Slider component

--- src/registry/new-york/molecules/color-picker/examples/demo.tsx
+++ src/registry/new-york/molecules/color-picker/examples/demo.tsx
@@ -1,30 +1,28 @@
-// import {
-//   ColorPicker,
-//   ColorPickerAlpha,
-//   ColorPickerEyeDropper,
-//   ColorPickerFormat,
-//   ColorPickerHue,
-//   ColorPickerOutput,
-//   ColorPickerSelection
-// } from '@/registry/new-york/molecules/color-picker/components/color-picker'
+import {
+  ColorPicker,
+  ColorPickerAlpha,
+  ColorPickerEyeDropper,
+  ColorPickerFormat,
+  ColorPickerHue,
+  ColorPickerOutput,
+  ColorPickerSelection
+} from '@/components/molecules/color-picker'
 
-// // Component
-// export const ColorPickerDemo = () => {
-//   // Template
-//   return (
-//     <ColorPicker className='w-sm rounded-md border bg-background p-4 shadow-sm'>
-//       <ColorPickerSelection />
-//       <div className='flex items-center gap-2'>
-//         <ColorPickerEyeDropper />
-//         <div className='grow space-y-1'>
-//           <ColorPickerHue />
-//           <ColorPickerAlpha />
-//         </div>
-//       </div>
-//       <div className='flex items-center gap-2'>
-//         <ColorPickerFormat />
-//         <ColorPickerOutput />
-//       </div>
-//     </ColorPicker>
-//   )
-// }
+export function ColorPickerDemo() {
+  return (
+    <ColorPicker className='w-sm rounded-md border bg-background p-4 shadow-sm'>
+      <ColorPickerSelection />
+      <div className='flex items-center gap-4'>
+        <ColorPickerEyeDropper />
+        <div className='grow space-y-2'>
+          <ColorPickerHue />
+          <ColorPickerAlpha />
+        </div>
+      </div>
+      <div className='flex items-center gap-2'>
+        <ColorPickerFormat />
+        <ColorPickerOutput />
+      </div>
+    </ColorPicker>
+  )
+}
